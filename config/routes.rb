f0bf809c81be85cb4f017d5ffb13fd6b969dc0ad
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-06-21 18:28:44 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: 'lists#index'
-  root to: "movies#index"
+  root to: 'lists#index'
+  # root to: "movies#index"
 
   resources :movies, except: :index
   resources :lists
