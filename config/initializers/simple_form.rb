762bf5a32e775866c769c4bc297b487d04649a30
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-06-21 17:54:58 +0100

    commit

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -112,7 +112,7 @@ SimpleForm.setup do |config|
   # You can define the class to use on all labels. Default is nil.
   # config.label_class = nil
 
-  # You can define the default class to be used on forms. Can be overridden
+  # You can define the default class to be used on forms. Can be overriden
   # with `html: { :class }`. Defaulting to none.
   # config.default_form_class = nil
 
@@ -173,4 +173,5 @@ SimpleForm.setup do |config|
   # Defines validation classes to the input_field. By default it's nil.
   # config.input_field_valid_class = 'is-valid'
   # config.input_field_error_class = 'is-invalid'
+
 end
